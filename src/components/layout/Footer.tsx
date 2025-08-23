dba94d8b603d8f677766e958af88905d24ef96dd
Abdullahi Alhassan <abdallaahelhassani60@gmail.com>
2025-08-23 21:40:38 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="flex f

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,9 +2,9 @@ export function Footer() {
   return (
     <footer className="border-t mt-12 py-6 bg-primary text-primary-foreground">
       <div className="container mx-auto px-4 text-center">
-        <p>&copy; 2024 Nursing and Midwifery Council of Nigeria (NMCN) &ndash; All Rights Reserved</p>
+        <p>&copy; 2025 Nursing and Midwifery Council of Nigeria (NMCN) &ndash; All Rights Reserved</p>
         <p className="mt-2">
-          For support, please contact: <a href="mailto:support@nmcn.gov.ng" className="hover:underline">support@nmcn.gov.ng</a>
+          For support, please contact: <a href="mailto:examinersworkshop@nmcn.gov.ng" className="hover:underline">examinersworkshop@nmcn.gov.ng</a>
         </p>
       </div>
     </footer>

c27349818dc2af9f6e4345c11b75457d1a297371
Abdullahi Alhassan <abdallaahelhassani60@gmail.com>
2025-09-01 12:53:16 +0000

    remove the space in the body to fit in the entire content in the mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       <body className="font-body antialiased">
         <div className="flex flex-col min-h-screen">
           <Header />
-          <main className="flex-grow container mx-auto px-4 py-8">
+          <main className="flex-grow container mx-auto px-2 sm:px-4 py-8">
             {children}
           </main>
           <Footer />
